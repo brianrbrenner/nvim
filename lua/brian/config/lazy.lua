cfb6d006c2b735ee5b9e60d399ec894676c65622
Brian Brenner <bbrenne6@kent.edu>
2023-01-10 00:08:48 -0500

    back on my bs (gruvbox)

--- lua/brian/config/lazy.lua
+++ lua/brian/config/lazy.lua
@@ -15,13 +15,24 @@ vim.g.mapleader = " "
 
 require("lazy").setup({
 	-- the colorscheme should be available when starting Neovim
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		-- load the colorscheme here
+	-- 		vim.cmd([[colorscheme tokyonight-night]])
+	-- 	end,
+	-- },
+
+	-- colorscheme
 	{
-		"folke/tokyonight.nvim",
+		"RRethy/nvim-base16",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight-night]])
+			vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
 		end,
 	},
 
