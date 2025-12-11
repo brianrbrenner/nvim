8d7cf0c02d8f9f38e9b3d2e596b658444280a780
Brian Brenner <bbrenne6@kent.edu>
2025-12-10 21:48:24 -0500

    remove empty file

--- ftplugin/zig.lua
+++
@@ -1,8 +0,0 @@
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	callback = function(args)
--- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 		if client then
--- 			client.server_capabilities.semanticTokensProvider = nil
--- 		end
--- 	end,
--- })
